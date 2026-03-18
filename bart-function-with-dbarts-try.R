afafb06e034dea42c05da71ec1c38ee7ac108fb2
luisesessler <167985708+luisesessler@users.noreply.github.com>
2026-03-18 16:42:42 +0100

    Add files via upload

--- bart-function-with-dbarts-try.R
+++ bart-function-with-dbarts-try.R
@@ -27,6 +27,38 @@ bart_one_model_new <- function(y, z, X, results, true_ate, testdata = X){
   return(results)
 }
 
+
+bart_two_models_new <- function(y, z, X, results, true_ate, testdata = X){
+  X_mm <- model.matrix(~ . - 1, data = X)
+  X_df <- data.frame(X_mm)
+  data_mm <- data.frame(X_mm, y)
+  
+  treated <- data_mm[z == 1,]
+  control <- data_mm[z == 0,]
+  
+  # TODO: das vlt auslagern, da auch für PS Matching genutzt wird
+  formula_mm <- colnames(X_mm) %>% paste0(collapse = " + ")
+  bart_formula <- as.formula(paste0("y ~ ", formula_mm))
+  
+  # bart2 muss genutzt werden, weil nur dort n.samples spezifizert werden kann
+  # TODO: chains and samples erhöhen
+  bart_fit_treated <- bart2(formula = bart_formula, data = treated, keepTrees = TRUE, combineChains = TRUE,
+                            n.trees = 200, n.burn = 1000, n.samples = 2000, n.chains = 4)
+  bart_fit_control <- bart2(formula = bart_formula, data = control, keepTrees = TRUE, combineChains = TRUE,
+                            n.trees = 200, n.burn = 1000, n.samples = 2000, n.chains = 4)
+  
+  # In Sample prediction
+  # Prediction for all units
+  predictions_treated <- predict(bart_fit_treated, newdata = X_df, type = "ev") 
+  predictions_control <- predict(bart_fit_control, newdata = X_df, type = "ev")
+  
+  # Calcuate ITEs
+  predictions_ite <- predictions_treated - predictions_control
+  
+  results <- get_metrics(predictions_ite,  "BART_two_models", results, true_ate)
+  return(results)
+}
+
 bart_ps_bart_new <- function(y, z, X, results, true_ate, testdata = X){
   X_mm <- model.matrix(~ . - 1, data = X)
   data_ps_mm <- data.frame(X_mm, z)
@@ -59,15 +91,15 @@ bart_ps_bart_new <- function(y, z, X, results, true_ate, testdata = X){
   return(results)
 }
 
-bart_ps_ps_new <- function(y, z, X, results, true_ate, testdata = X){
+bart_ps_glm_new <- function(y, z, X, results, true_ate, testdata = X){
   X_mm <- model.matrix(~ . - 1, data = X)
   data_ps_mm <- data.frame(X_mm, z)
   
   formula_mm <- colnames(X_mm) %>% paste0(collapse = " + ")
   ps_formula <- as.formula(paste0("z ~ ", formula_mm))
   
-  ps_fit <- bart2(formula = ps_formula, data = data_ps_mm, family = "binomial", keepTrees = TRUE, combineChains = TRUE)
-  ps <- predict(ps_fit, data_ps_mm, type = "ppd") %>% colMeans()
+  ps_fit <- glm(formula = ps_formula, data = data_ps_mm, family = "binomial")
+  ps <- predict(ps_fit, data_ps_mm, type = "ev") %>% colMeans()
   
   data_mm <- data.frame(X_mm, ps, z, y)
   
@@ -79,14 +111,14 @@ bart_ps_ps_new <- function(y, z, X, results, true_ate, testdata = X){
   data_mm_treated <- data.frame(X_mm, ps = ps, z = 1)
   data_mm_control <- data.frame(X_mm, ps = ps, z = 0)
   
-  predictions_treated <- predict(bart_fit, newdata = data_mm_treated, type = "ppd") 
-  predictions_control <- predict(bart_fit, newdata = data_mm_control, type = "ppd")
+  predictions_treated <- predict(bart_fit, newdata = data_mm_treated, type = "ev") 
+  predictions_control <- predict(bart_fit, newdata = data_mm_control, type = "ev")
   
   #----
   # In sample metrics
   # Predict each observation in treatment and in control group
   
   predictions_ite <- predictions_treated - predictions_control
-  results <- get_metrics(predictions_ite, "BART_ps_bart", results, true_ate)
+  results <- get_metrics(predictions_ite, "BART_ps_glm", results, true_ate)
   return(results)
 }
---
+++ bart_t-learner.R
@@ -0,0 +1,43 @@
+# Mein Computer
+PATH_DATASETS <- "C:/Users/luise/Documents/Masterarbeit/Daten/"  
+PATH_TRUE_ATES <- "C:/Users/luise/Documents/Masterarbeit/Daten/lowDim_trueATE.csv"  
+PATH_OVERVIEW <- "C:/Users/luise/Documents/Masterarbeit/Daten/dgp_overview.csv" 
+
+# Uni-Computer
+# PATH_DATASETS <- "W:/Masterarbeit/Daten/"  
+# PATH_TRUE_ATES <- "W:/Masterarbeit/Daten/trueATE/lowDim_trueATE.csv"  
+# PATH_OVERVIEW <- "W:/Masterarbeit/Daten/trueATE/dgp_overview.csv" 
+# PATH_RESULTS <- "W:/Masterarbeit/Results/" 
+
+dgpis <- read.csv(PATH_OVERVIEW)
+all_datasets <- read.csv(PATH_TRUE_ATES)
+cont_dgpis <- dgpis %>% filter(binary == 0)
+
+all_results <- list()
+all_summaries <- list()
+
+set.seed(213)
+
+#for(i in 1:length(cont_dgpis)){
+for(i in 1){
+  dgp_index <- cont_dgpis$DGPid[i]
+  true_ate <- cont_dgpis$trueATE[i]
+  
+  results <- make_results_list()
+  
+  datasets_dgp <- all_datasets %>% filter(DGPid == dgp_index)
+  
+  #for (j in 1:nrow(datasets_dgp)){
+  for (j in 1:1){
+    file_name <- datasets_dgp[j, "filename"]
+    data <- read.csv(paste0(PATH_DATASETS, file_name, ".csv"))
+    y <- data$Y
+    z <- data$A
+    X <- data %>% select(starts_with("V"))
+    
+    results <- bart_two_models_new(y, z, X, results, true_ate)
+  }
+  
+  all_results[[paste0("dgp", dgp_index)]] <- results
+  all_summaries[[paste0("dgp", dgp_index)]] <- create_results_table(results)
+}
